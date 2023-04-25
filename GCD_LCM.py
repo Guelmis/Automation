579d49d6b0d757fc18b95e52b4061868c42840a2
unknown <guelmisedwards@gmail.com>
2023-04-25 11:40:10 -0400

    Testing First Python Code

---
+++ GCD_LCM.py
@@ -0,0 +1,14 @@
+import math
+x = 15
+'''int(input("Enter the first number: "))'''
+y = 25
+'''int(input("Enter the second number: "))'''
+GCD = math.gcd(x,y)
+GCDS = str(GCD)
+print("The GCD of the provided number is "+GCDS)
+'''x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+'''
+LCM = math.lcm(x,y)
+LCMS = str(LCM)
+print("The LCM of the provided number is "+LCMS)
